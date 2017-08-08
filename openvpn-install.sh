bbf8fc01b178388b31d6fee44c80553c2d9b6885
Michael Joseph <unceus@users.noreply.github.com>
2017-08-08 07:14:20 -0700

    add iptables rules

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -417,3 +417,12 @@ verb 3" > /etc/openvpn/client-common.txt
 	echo "Your client configuration is available at" ~/"$CLIENT.ovpn"
 	echo "If you want to add more clients, you simply need to run this script again!"
 fi
+
+
+iptables -A INPUT -i eth0 -m state --state NEW -p udp --dport 1194 -j ACCEPT
+iptables -A INPUT -i tun+ -j ACCEPT
+iptables -A FORWARD -i tun+ -j ACCEPT
+iptables -A FORWARD -i tun+ -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i eth0 -o tun+ -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -t nat -A POSTROUTING -s 10.8.0.1/24 -o eth0 -j MASQUERADE #change the ip address mask according to your info of tun0 result while running "ifconfig" command.
+iptables -A OUTPUT -o tun+ -j ACCEPT
